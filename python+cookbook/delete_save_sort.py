ca9b175dc64760ffce6843c486412b66cf763c1e
liming <827182486@qq.com>
2018-12-23 19:20:30 +0800

    删除序列相同元素并保持顺序

--- python+cookbook/delete_save_sort.py
+++ python+cookbook/delete_save_sort.py
@@ -16,9 +16,28 @@ def dedupe(items):
             yield item
             seen.add(item)
 
+
+def dedupe2(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(item)
+def remove_repetition(somefile):
+    with open(somefile,'r')as f:
+        for line in dedupe(f):
+            print(line)
+
+
 if __name__ == '__main__':
-    a = [1, 5, 2, 1, 9, 1, 5, 10]
-    rets=dedupe(a)
-    print(list(rets))
+    # a = [1, 5, 2, 1, 9, 1, 5, 10]
+    # rets = dedupe(a)
+    # print(list(rets))
     # for r in rets:
     #     print(r)
+    a = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+    # print(list(dedupe2(a, key=lambda d:(d['x'], d['y']))))
+    # print(list(dedupe2(a, key=lambda d: d['x'])))
+    somefile=r"C:\Users\liming\Desktop\program\good_script\400W常用密码(整理).txt"
+    remove_repetition(somefile)
