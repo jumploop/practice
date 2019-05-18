df38f58d48a08535601abad78b8c8c1070799983
liming <827182486@qq.com>
2019-05-18 20:05:53 +0800

    简单发送邮件

--- good_script/simple_send_email.py
+++ good_script/simple_send_email.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import yagmail
+
+# 链接邮箱服务器
+yag = yagmail.SMTP(user='982698078@qq.com', password='****', host='smtp.qq.com')
+# 邮件正文
+contents = ['this is a python send mail test!', 'you can find a apple', 'apple']
+# 发送邮件
+resp = yag.send('827182486@qq.com', 'object', contents)
+print(resp)
